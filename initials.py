df222f0e45a84a391462fd14c3cdd151cfa60729
divinemakafui2099-alt <divinemakafui2099@gmail.com>
2026-05-31 22:07:55 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
+# File: initials.py
+# Description: Prints stylized large initials DMH with required spacing and period grids.
+# Assignment Number: 1
+#
+# Name: Divine Makafui Honkou
+# STUDENT ID: 2425402973
+# Email: divinemakafui2099@gmail.com
+# Grader: Mr.Augustus Buckman
+#
+# On my honor, Divine Makafui Honkou, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
-
+    # This function prints the stylized DMH initials using precise block dimensions.
+    print()
+    print("...DMH")
+    print()
+    print("......DDDDDDDDD.......MMMM......MMMM...HHHH......HHHH......")
+    print("......DDDDDDDDDD......MMMMMM..MMMMMM...HHHH......HHHH......")
+    print("......DDDD....DDDD....MMMMMMMMMMMMMM...HHHH......HHHH......")
+    print("......DDDD.....DDDD...MMMM.MMMM.MMMM...HHHH......HHHH......")
+    print("......DDDD.....DDDD...MMMM..MM..MMMM...HHHHHHHHHHHHHH......")
+    print("......DDDD.....DDDD...MMMM......MMMM...HHHHHHHHHHHHHH......")
+    print("......DDDD.....DDDD...MMMM......MMMM...HHHH......HHHH......")
+    print("......DDDD....DDDD....MMMM......MMMM...HHHH......HHHH......")
+    print("......DDDDDDDDDD......MMMM......MMMM...HHHH......HHHH......")
+    print("......DDDDDDDDD.......MMMM......MMMM...HHHH......HHHH.*....")
+    print("......DDDDDDDDD.......MMMM......MMMM...HHHH......HHHH.*....")
+    print()
 
 
 main()
